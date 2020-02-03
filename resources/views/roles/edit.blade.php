086cd40ed3ca7b9c0e2a105143c5539c9925be9e
Shadrack-B <shadrackbakasa5@gmail.com>
2020-02-03 16:13:53 +0300

    role crud

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="content-header">
         <h1>
-            Role
+            <i class="fa fa-registered">Role</i> 
         </h1>
    </section>
    <div class="content">
@@ -11,12 +11,20 @@
        <div class="box box-primary">
            <div class="box-body">
                <div class="row">
-                   {!! Form::model($role, ['route' => ['roles.update', $role->id], 'method' => 'patch']) !!}
+                   {!! Form::model($role, ['route' => ['roles.update', $role->role_id], 'method' => 'patch']) !!}
 
-                        @include('roles.fields')
+                        <!-- Name Field -->
+                        <div class="form-group col-md-6">
+                            {!! Form::label('name', 'Name:') !!}
+                            {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                        </div>
+                </div>
+
+                <div>
+                    {!! Form::submit('Update Role', ['class' => 'btn btn-info']) !!}
+                </div>
 
                    {!! Form::close() !!}
-               </div>
            </div>
        </div>
    </div>
